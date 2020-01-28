fef265006ba93782d34c85b06c8a3cf0664aa8b2
Tulir Asokan <tulir@maunium.net>
2020-01-28 22:49:32 +0200

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -7,5 +7,15 @@ fixperms() {
 
 cd /opt/mautrix-manager/backend
 
+if [ ! -f /data/config.yaml ]; then
+	cp example-config.yaml /data/config.yaml
+	echo "Didn't find a config file."
+	echo "Copied default config file to /data/config.yaml"
+	echo "Modify that config file to your liking."
+	echo "Start the container again after that to generate the registration file."
+	fixperms
+	exit
+fi
+
 fixperms
-exec su-exec $UID:$GID python3 -m mautrix_telegram -c /data/config.yaml
+exec su-exec $UID:$GID python3 -m mautrix_manager -c /data/config.yaml
