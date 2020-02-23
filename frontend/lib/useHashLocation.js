9d307dd76217ce8af4d35b8c61351450d1ed540a
Tulir Asokan <tulir@maunium.net>
2020-02-24 00:04:11 +0200

    Add hacky way to get query params out of hash URL

--- frontend/lib/useHashLocation.js
+++ frontend/lib/useHashLocation.js
@@ -1,7 +1,37 @@
 // From https://github.com/molefrog/wouter#customizing-the-location-hook
+import { createRef } from "../web_modules/preact.js"
 import { useState, useEffect, useCallback } from "../web_modules/preact/hooks.js"
 
-const currentLocation = () => window.location.hash.replace(/^#/, "") || "/"
+const parseQuery = str => Object.fromEntries(
+    str.split("&")
+        .map(part => part.split("="))
+        .map(([key, value = ""]) => [key, value]))
+
+const setQuery = query => {
+    const [start] = window.location.hash.split("?", 1)
+    const str = new URLSearchParams(query).toString()
+    if (str.length > 0) {
+        window.location.hash = `start?${str}`
+    } else {
+        window.location.hash = start
+    }
+}
+
+const query = createRef()
+query.current = {}
+
+export const useQuery = () => [query.current, setQuery]
+
+const currentLocation = () => {
+    const newLocation = window.location.hash.replace(/^#/, "") || "/"
+    const queryIndex = newLocation.indexOf("?")
+    if (queryIndex !== -1) {
+        query.current = parseQuery(newLocation.substr(queryIndex + 1))
+        return newLocation.substr(0, queryIndex)
+    } else {
+        return newLocation
+    }
+}
 
 const useHashLocation = () => {
     const [location, setLocation] = useState(currentLocation())
